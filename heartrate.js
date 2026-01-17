01671cb6d5c46c759325592f9bc82f716eeacae0
Gergely Imreh <gergely@imreh.net>
2026-01-17 18:14:27 +0800

    update navigation code

--- heartrate.js
+++ heartrate.js
@@ -13,7 +13,9 @@ function waitForElement(classname, callback) {
 // Data Management
 
 function getData() {
-  let dialog = window.confirm("Getting data?");
+  let dialog = window.confirm(
+    "Download this day's heartrate data as a CSV file?",
+  );
   if (dialog) {
     const pathParts = window.location.pathname.split("/");
     // The path is something like "/app/heart-rate/2026-01-16/0" resulting in
@@ -67,16 +69,43 @@ function exportHeartRateData(date, heartrateData) {
   a.href = url;
   a.download = `heartrate-${date}.csv`;
   a.click();
-  URL.revokeObjectURL;
+  URL.revokeObjectURL(url);
 }
 
-// TODO: should only happen on the pages which we can export
-// TODO: need to re-run this on navigation
-waitForElement("headerComponent-headerTitleContainer-area", (header) => {
-  const btn = document.createElement("button");
-  btn.textContent = "Export";
-  btn.type = "button";
-  btn.style.border = "1px solid black";
-  btn.addEventListener("click", getData);
-  header.appendChild(btn);
-});
+function onNavigate() {
+  // Check if we've added our button to the page already
+  const existing_btn = document.querySelector(".extra-export-btn");
+
+  // Match the single day path /app/heart-rate/2026-01-15 or /app/heart-rate/2026-01-15/0
+  target_path = /^\/app\/heart-rate\/\d{4}-\d{2}-\d{2}(?:\/?|\/0)$/g;
+
+  if (window.location.pathname.match(target_path)) {
+    if (existing_btn) return;
+
+    waitForElement("headerComponent-headerTitleContainer-area", (header) => {
+      const btn = document.createElement("button");
+      btn.textContent = "Export";
+      btn.type = "button";
+      btn.className = "extra-export-btn";
+      // TODO: do styling separately
+      btn.style.border = "1px solid black";
+      btn.addEventListener("click", getData);
+      header.appendChild(btn);
+    });
+  } else {
+    if (existing_btn) {
+      existing_btn.remove();
+    }
+  }
+}
+
+// Check if the Navigation API is supported
+if ("navigation" in window) {
+  window.navigation.addEventListener("navigatesuccess", (event) => {
+    onNavigate();
+  });
+} else {
+  console.log("Navigation API not supported, using fallback methods.");
+}
+// Kick it off on the first load; Navigation API doesn't trigger then.
+onNavigate();
